e7d4690e6423aba004953f8000b20ef75605e8ca
Bermely Ferreras <bfbferreras@gmail.com>
2023-10-02 21:13:08 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(rightNumbers, 10);
+  
+    // Define the width of the container (adjust as needed)
+    const containerWidth = 400; // You should replace this with the actual container width
+  
+    // Define the DODGER's width (adjust as needed)
+    const dodgerWidth = 40; // You should replace this with the actual DODGER width
+  
+    // Calculate the maximum allowed right position
+    const maxRightPosition = containerWidth - dodgerWidth;
+  
+    if (right < maxRightPosition) {
+      dodger.style.left = `${right + 1}px`;
+    }
+  }
+  
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  
